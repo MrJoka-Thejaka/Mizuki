b09015033950f583f476d0227fe984a9f1d2d22d
Mr.Joka <84589571+MrJoka-Thejaka@users.noreply.github.com>
2021-09-16 14:43:31 +0530

    Add files via upload

---
+++ plugins/sql/notes.js
@@ -0,0 +1,42 @@
+/*
+# Copyright (C) 2020 MuhammedKpln. /  Copyright (C) 2021 Mr.joka
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+*/
+
+const config = require('../../config');
+const { DataTypes } = require('sequelize');
+
+const NotesDB = config.DATABASE.define('notes', {
+    note: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+
+async function getNotes() {
+    const Notes = await NotesDB.findAll()
+
+    return Notes
+}
+
+async function saveNote(note) {
+    return await NotesDB.create({ note });
+}
+
+async function deleteAllNotes() {
+    return await NotesDB.destroy({
+        where: {},
+        truncate: true
+    })
+}
+
+module.exports = {
+    NotesDB,
+    getNotes,
+    saveNote,
+    deleteAllNotes
+};
