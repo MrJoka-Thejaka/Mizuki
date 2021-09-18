372264438aad104cc2bf932221cb120cb51cfdfe
Mr.Joka <84589571+MrJoka-Thejaka@users.noreply.github.com>
2021-09-18 18:05:31 +0530

    👸 Mizuki 1.0 beta version
    Song uploading filetype changed to doc

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -326,7 +326,7 @@ if (config.WORKTYPE == 'private') {
                 writer.addTag();
 
                 reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG,MessageType.text);
-                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
+                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.documents, {mimetype: Mimetype.audio/mpeg, ptt: false});
             });
     }));
 
@@ -793,7 +793,7 @@ else if (config.WORKTYPE == 'public') {
                 writer.addTag();
 
                 reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG,MessageType.text);
-                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
+                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.documents, {mimetype: Mimetype.audio/mpeg, ptt: false});
             });
     }));
 
