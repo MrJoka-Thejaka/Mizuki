dcd25d5b65b0ed7dbd8e6559ab93c942541806b4
Mr.Joka <84589571+MrJoka-Thejaka@users.noreply.github.com>
2021-09-20 09:12:58 +0530

    yt command new look #2

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -372,7 +372,7 @@ if (config.WORKTYPE == 'private') {
     
         var mesaj = '';
         arama.all.map((video) => {
-            mesaj += '📽️ *' + video.title + '*🔗 ' + video.url + '\n'
+            mesaj += '📽️ *' + video.title + '*🔗 ' + video.url + '\n\n'
         });
 
         await message.client.sendMessage(message.jid,mesaj,MessageType.text);
@@ -592,7 +592,7 @@ else if (config.WORKTYPE == 'public') {
     
         var mesaj = '';
         arama.all.map((video) => {
-            mesaj += '📽️ *' + video.title + '*🔗 ' + video.url + '\n'
+            mesaj += '📽️ *' + video.title + '*🔗 ' + video.url + '\n\n'
         });
 
         await message.client.sendMessage(message.jid,mesaj,MessageType.text);
