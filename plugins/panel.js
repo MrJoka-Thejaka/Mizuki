4552ceaad9ab7a6c1d01faf986f00fd7e1ca9f97
Mr.Joka <84589571+MrJoka-Thejaka@users.noreply.github.com>
2021-09-24 23:02:09 +0530

    New command list command added you can use *.panel* now for the command list.

---
+++ plugins/panel.js
@@ -0,0 +1,230 @@
+/* Copyright (C) 2021 Sisula / Copyright (C) 2021 Mr.joka.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Mizuki - Mr.joka
+*/
+
+const Mizuki = require('../events');
+const Config = require('../config');
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Language = require('../language');
+const Lang = Language.getString('_mizuki');
+
+if (Config.WORKTYPE == 'private') {
+
+    Mizuki.addCommand({pattern: 'panel ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+        var CMD_HELP = '';
+        if (match[1] === '') {
+            Mizuki.commands.map(
+                async (command) =>  {
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+                    try {
+                        var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
+                        var mmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2]
+                    } catch {
+                        var match = [command.pattern];
+                    }
+    
+                    var HANDLER = '';
+    
+                    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                        HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                    } else {
+                        HANDLER = '.';
+                    }
+                    if (command.desc == '' && !command.usage == '' && command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                    }
+                    if (!command.desc == '' && command.usage == '' && command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n\n';
+                    }
+                    if (command.desc == '' && command.usage == '' && !command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                    if (!command.desc == '' && !command.usage == '' && command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                    }
+                    if (!command.desc == '' && command.usage == '' && !command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                    if (command.desc == '' && !command.usage == '' && !command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                    if  (command.desc == '' && command.usage == '' && command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n\n'
+                    }
+                    if  (!command.desc == '' && !command.usage == '' && !command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                }
+            );
+            await message.client.sendMessage(
+                message.jid,'●▬▬▬ *Mizuki Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+            );    
+        } else {
+            var CMD_HELP = '';
+            Mizuki.commands.map(
+                async (command) =>  {
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+                    try {
+                        var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
+                        var cmmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2]
+                    } catch {
+                        var cmatch = [command.pattern];
+                    }
+                    if (cmmatch.endsWith(' ')) {
+                        var cmmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2].replace(' ', '')
+                    }
+                    if (cmmatch == match[1]) {
+                        var HANDLER = '';
+    
+                        if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                            HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                        } else {
+                            HANDLER = '.';
+                        }
+                        if (command.desc == '' && !command.usage == '' && command.warn == '') {
+                        CMD_HELP += Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                        }
+                        if (!command.desc == '' && command.usage == '' && command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n\n';
+                        }
+                        if (command.desc == '' && command.usage == '' && !command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                        if (!command.desc == '' && !command.usage == '' && command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*' + Config.D_EMOJI + + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                        }
+                        if (!command.desc == '' && command.usage == '' && !command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                        if (command.desc == '' && !command.usage == '' && !command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                        if  (command.desc == '' && command.usage == '' && command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n\n'
+                        }
+                        if  (!command.desc == '' && !command.usage == '' && !command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                    }
+                }
+            );
+            if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+            await message.client.sendMessage(
+                message.jid,'●▬▬▬ *Mizuki Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+            );
+        }
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Mizuki.addCommand({pattern: 'panel ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+        var CMD_HELP = '';
+        if (match[1] === '') {
+            Mizuki.commands.map(
+                async (command) =>  {
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+                    try {
+                        var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
+                        var mmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2]
+                    } catch {
+                        var match = [command.pattern];
+                    }
+    
+                    var HANDLER = '';
+    
+                    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                        HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                    } else {
+                        HANDLER = '.';
+                    }
+                    if (command.desc == '' && !command.usage == '' && command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                    }
+                    if (!command.desc == '' && command.usage == '' && command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n\n';
+                    }
+                    if (command.desc == '' && command.usage == '' && !command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                    if (!command.desc == '' && !command.usage == '' && command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                    }
+                    if (!command.desc == '' && command.usage == '' && !command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                    if (command.desc == '' && !command.usage == '' && !command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                    if  (command.desc == '' && command.usage == '' && command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n\n'
+                    }
+                    if  (!command.desc == '' && !command.usage == '' && !command.warn == '') {
+                        CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                }
+            );
+            await message.client.sendMessage(
+                message.jid,'●▬▬▬ *Mizuki Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+            );    
+        } else {
+            var CMD_HELP = '';
+            Mizuki.commands.map(
+                async (command) =>  {
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+                    try {
+                        var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
+                        var cmmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2]
+                    } catch {
+                        var cmatch = [command.pattern];
+                    }
+                    if (cmmatch.endsWith(' ')) {
+                        var cmmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2].replace(' ', '')
+                    }
+                    if (cmmatch == match[1]) {
+                        var HANDLER = '';
+    
+                        if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                            HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                        } else {
+                            HANDLER = '.';
+                        }
+                        if (command.desc == '' && !command.usage == '' && command.warn == '') {
+                        CMD_HELP += Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                        }
+                        if (!command.desc == '' && command.usage == '' && command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n\n';
+                        }
+                        if (command.desc == '' && command.usage == '' && !command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                        if (!command.desc == '' && !command.usage == '' && command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*' + Config.D_EMOJI + + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                        }
+                        if (!command.desc == '' && command.usage == '' && !command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                        if (command.desc == '' && !command.usage == '' && !command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                        if  (command.desc == '' && command.usage == '' && command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n\n'
+                        }
+                        if  (!command.desc == '' && !command.usage == '' && !command.warn == '') {
+                            CMD_HELP += '*' + Config.C_EMOJI + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmmatch) : command.pattern) + '```\n' + '*' + Config.D_EMOJI + Lang.DESC + ':* ```' + command.desc + '``` \n' + '*' + Config.D_EMOJI + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                    }
+                }
+            );
+            if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+            await message.client.sendMessage(
+                message.jid,'●▬▬▬ *Mizuki Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+            );
+        }
+    }));
+}
