d9159b8abd273de29a150f0af8c33cb2c9f2d3a3
Mr.Joka <84589571+MrJoka-Thejaka@users.noreply.github.com>
2021-10-09 06:19:08 +0530

    Don't update this is a test

--- config.js
+++ config.js
@@ -33,7 +33,6 @@ module.exports = {
     BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
     ADDMSG: process.env.ADD_MESSAGE === undefined ? 'default' : process.env.ADD_MESSAGE,
     MUTEMSG: process.env.MUTE_MESSAGE === undefined ? 'default' : process.env.MUTE_MESSAGE,
-    KEY: process.env.KEY === undefined ?'404Api' : process.env.KEY,
     NOLOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
     FULLEVA: process.env.FULL_EVA === undefined ? 'false' : process.env.FULL_EVA,
     BLOCKMSG: process.env.BLOCK_MESSAGE === undefined ? 'default' : process.env.BLOCK_MESSAGE,
