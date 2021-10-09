62ba1b2590dd5e06e10954fba9fff2a2aaad1167
Mr.Joka <84589571+MrJoka-Thejaka@users.noreply.github.com>
2021-10-09 12:45:38 +0530

    please install this plugin like this :- .install https://gist.github.com/MrJoka-Thejaka/8f6a0f43e22502d238b906bc3e9c62a1

--- plugins/bottt.js
+++ plugins/bottt.js
@@ -1,13 +1,1 @@
-const Mizuki = require('../events');
-const { MessageType } = require('@adiwajshing/baileys');
-const Config = require('../config');
-let workt = Config.WORKTYPE == 'public' ? false : true
-const PB_DESC = '.'
-const msg = '```🗝️ Please don\'t use``` *.bot* ```to chat with me\n🎗️ Just mention me to chat with me.```'
-
-Mizuki.addCommand({pattern: 'bot ?(.*)', fromMe: workt, desc: PB_DESC, dontAddCommandList: true}, (async (message, match) => {  
-  
-    await message.client.sendMessage(message.jid,msg,MessageType.text, {quoted: message.data});
-
-
-}))
+//https://gist.github.com/MrJoka-Thejaka/8f6a0f43e22502d238b906bc3e9c62a1
