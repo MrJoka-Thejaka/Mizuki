ca194ad17e7cd9a2e7c5136840a2569faf164817
Mr.Joka <84589571+MrJoka-Thejaka@users.noreply.github.com>
2021-10-15 11:34:51 +0530

    clean.js Decrypt | Don't update | revert #1

--- plugins/clean.js
+++ plugins/clean.js
@@ -1,3 +1,19 @@
-//මේක උස්සන්න බෑ සගෝ ඔයා යම්න
+const { MessageType, GroupSettingChange, ChatModification, WAConnectionTest } = require('@adiwajshing/baileys');
+const Mizuki = require('../events');
+const Config = require('../config');
+const CLEAR_DESC = "Clears all the messages in the commanded group"
 
-const _0x2e7d49=_0x970a;function _0x5875(){const _0x4d9c9f=['21rQBUsK','2585720AYQYNE','includes','```cleaning\x20chat...\x20🧹```','3905488NJAXvf','504JkLJOq','@adiwajshing/baileys','```Chat\x20cleared\x20🚮```','31000662sYZOYK','sendMessage','6SvevSg','user','addCommand','map','modifyChat','client','12OopiKB','participants','jid','isAdmin','167284DZwbkx','186530GaYuvN','280096QDFcRf','split','346696cidqAd','clear','Clears\x20all\x20the\x20messages\x20in\x20the\x20commanded\x20group'];_0x5875=function(){return _0x4d9c9f;};return _0x5875();}function _0x970a(_0x171aa7,_0x3bdd3a){const _0x587573=_0x5875();return _0x970a=function(_0x970a2a,_0x5ed615){_0x970a2a=_0x970a2a-0xd8;let _0x17bf93=_0x587573[_0x970a2a];return _0x17bf93;},_0x970a(_0x171aa7,_0x3bdd3a);}(function(_0x189963,_0x450e28){const _0x3d6a0b=_0x970a,_0x48dc00=_0x189963();while(!![]){try{const _0x3817dd=parseInt(_0x3d6a0b(0xe2))/0x1+parseInt(_0x3d6a0b(0xde))/0x2*(parseInt(_0x3d6a0b(0xef))/0x3)+parseInt(_0x3d6a0b(0xe9))/0x4+parseInt(_0x3d6a0b(0xe6))/0x5*(parseInt(_0x3d6a0b(0xda))/0x6)+parseInt(_0x3d6a0b(0xe5))/0x7*(-parseInt(_0x3d6a0b(0xe0))/0x8)+parseInt(_0x3d6a0b(0xea))/0x9*(parseInt(_0x3d6a0b(0xdf))/0xa)+-parseInt(_0x3d6a0b(0xed))/0xb;if(_0x3817dd===_0x450e28)break;else _0x48dc00['push'](_0x48dc00['shift']());}catch(_0x354db8){_0x48dc00['push'](_0x48dc00['shift']());}}}(_0x5875,0x9db2a));const {MessageType,GroupSettingChange,ChatModification,WAConnectionTest}=require(_0x2e7d49(0xeb)),Mizuki=require('../events'),Config=require('../config'),CLEAR_DESC=_0x2e7d49(0xe4);async function checkImAdmin(_0x4958b9,_0x413957=_0x4958b9[_0x2e7d49(0xd9)][_0x2e7d49(0xf0)][_0x2e7d49(0xdc)]){const _0x2feb5f=_0x2e7d49;var _0x5cf903=await _0x4958b9[_0x2feb5f(0xd9)]['groupMetadata'](_0x4958b9['jid']),_0x1418c1=_0x5cf903[_0x2feb5f(0xdb)][_0x2feb5f(0xf2)](_0x3cb134=>{const _0x5269d4=_0x2feb5f;if(_0x3cb134['id'][_0x5269d4(0xe1)]('@')[0x0]===_0x413957[_0x5269d4(0xe1)]('@')[0x0]&&_0x3cb134[_0x5269d4(0xdd)])return!![];else;return![];});return _0x1418c1[_0x2feb5f(0xe7)](!![]);}Mizuki[_0x2e7d49(0xf1)]({'pattern':_0x2e7d49(0xe3),'fromMe':!![],'desc':CLEAR_DESC,'dontAddCommandList':![]},async(_0x39b3e2,_0x29da35)=>{const _0x1a0f85=_0x2e7d49;await _0x39b3e2[_0x1a0f85(0xee)](_0x1a0f85(0xe8)),await _0x39b3e2[_0x1a0f85(0xd9)][_0x1a0f85(0xd8)](_0x39b3e2['jid'],ChatModification['delete']),await _0x39b3e2[_0x1a0f85(0xee)](_0x1a0f85(0xec));});
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {
+        if (member.id.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+
+Mizuki.addCommand({ pattern: 'clear', fromMe: true, desc: CLEAR_DESC, dontAddCommandList: false }, (async (message, match) => {
+
+    await message.sendMessage('```cleaning chat... 🧹```');
+    await message.client.modifyChat(message.jid, ChatModification.delete);
+    await message.sendMessage('```Chat cleared 🚮```');
+}));
