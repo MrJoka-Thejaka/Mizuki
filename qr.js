825518f4a7cc82b8a94d67067dcf0749bd6d00ef
Mr.Joka <84589571+MrJoka-Thejaka@users.noreply.github.com>
2021-10-16 17:51:17 +0530

    Don't update | Termux Qr gen updated

--- qr.js
+++ qr.js
@@ -7,53 +7,67 @@ you may not use this file except in compliance with the License.
 */
 
 const chalk = require('chalk');
-const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
-const {StringSession} = require('./whatsasena/');
+const {WAConnection} = require('@adiwajshing/baileys');
+const {StringSession} = require('./Mizuki/');
 const fs = require('fs');
 
-async function whatsAsena () {
+async function Mizuki () {
     const conn = new WAConnection();
     const Session = new StringSession();  
-    conn.version = [2, 2119, 6]
+    conn.version = [2, 2126, 14]
     conn.logger.level = 'warn';
     conn.regenerateQRIntervalMs = 50000;
     
-    conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('MIZUKI')}
-${chalk.white.italic('Mizuki Code Recipient')}
-
-${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait. I am 👸 Mizuki')}`);
+    conn.on('🔄 connecting', async () => {
+        console.log(`${chalk.green.bold('Queen')}${chalk.blue.bold(' Amdi')}
+${chalk.white.italic('Queen Amdi Sting session')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     });
     
 
-    conn.on('open', async () => {
-        var st = Session.createStringSession(conn.base64EncodedAuthInfo());
-        console.log(
-            chalk.green.bold('Your Mizuki String Code: '), Session.createStringSession(conn.base64EncodedAuthInfo())
-        );
-        
-        if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `MIZUKI_SESSION="${st}"`);
-        }
-        if (conn.user.jid.startsWith('90')) {
-            await conn.sendMessage(conn.user.jid,st, MessageType.text)
-            await conn.sendMessage(conn.user.jid,'*Do Not Share The Code With Anyone!*', MessageType.text)
-            console.log(
-                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js.')
-            );
-        }
-        else {
-            await conn.sendMessage(conn.user.jid,st, MessageType.text)
-            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!*', MessageType.text)
-            console.log(
-                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js')
-            );
-        }
-        
-        process.exit(0);
-    });
+	conn.on('open', async () => {
+		console.log(
+			chalk.green.bold('Queen Mizuki QR Code: '),
+			'MIZUKI;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				)
+		);
+		await conn.sendMessage(
+			conn.user.jid,
+			'MIZUKI;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				),
+			MessageType.text
+		);
+		if (conn.user.jid.startsWith('94')) {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ මේක කාටවත් දෙන්න එපා !! ; I am Queen 𝐌𝐈𝐙𝐔𝐊𝐈 👸 ' + conn.user.name + '* 🛑',
+				MessageType.text
+			);
+		} else {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Please Do Not Share This Code With Anyone  !! ; I am Queen 𝐌𝐈𝐙𝐔𝐊𝐈 👸 ' +
+					conn.user.name +
+					'* 🛑',
+				MessageType.text
+			);
+		}
+		console.log(
+			chalk.green.bold(
+				"Meka copy karanna bari nm whatsapp eke athi bn code eka awith!\n"
+			),
+			chalk.green.bold(
+				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+			)
+		);
+		process.exit(0);
+	});
 
-    await conn.connect();
+	await conn.connect();
 }
 
-whatsAsena()
+Mizuki()
